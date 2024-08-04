ea991f9e22d3afa8faccd92b5e9e425b14205864
Ali Sa'adati <aleesduct@gmail.com>
2024-08-04 10:26:15 +0800

    app.js and jsx manipulation

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleRandomNames = () => {
+    const name = ['Jamie', 'Dwayne', 'Chris'];
+    const int = Math.floor(Math.random() * 3);
+    return name[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to see loading.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        {<p>Hi, {handleRandomNames()}</p>}
       </header>
     </div>
   );
