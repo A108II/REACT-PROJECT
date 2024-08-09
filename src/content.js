b99d273ddabfd163060bfdc1d7e3f0bdeb42cbfe
Ali Sa'adati <aleesduct@gmail.com>
2024-08-09 11:10:18 +0800

    click events

--- src/content.js
+++ src/content.js
@@ -4,12 +4,30 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return name[int];
   }
+
+  const handleClick = () => {
+    console.log('You clicked it');
+  }
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  }
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  }
+
   return (
     <main>
-      <p>
+      <p onDoubleClick={handleClick}>
         Hello {handleRandomNames()}
       </p>
-      
+      {/* 
+      - If we put the operators (), the function gets called immediately without clicking the button
+      - Anonymous function () => {} only gets called after the onclick and function inside which is handleClick2('Hulk')immediately gets called.
+      */}
+      <button onClick={handleClick}>Click</button>
+      <button onClick={() => handleClick2('Hulk')}>Click</button>
+      {/* Acceessing event object when clicking event */}
+      <button onClick={(e) => handleClick3(e)}>Click</button>
     </main>
   )
 }
